56db1708ea84894e220ebf43ef5b67c8b6955511
Nikolaj Quorning <nikolaj.quorning@gmail.com>
2016-02-16 08:55:13 +0100

    Introduktion

---
+++ Introduktion/Introduktion/main.asm
@@ -0,0 +1,38 @@
+;********** MSYS, LAB1 **********
+;******* Henning Hargaard *******
+;*******  14.august 2015  *******
+;********************************
+
+;********** INITIERING **********
+   LDI  R16,HIGH(RAMEND) ;Initialize Stack Pointer
+   OUT  SPH,R16
+   LDI  R16,LOW(RAMEND)
+   OUT  SPL,R16           
+   SER  R16              ;PORTB = Outputs
+   OUT  DDRB,R16
+
+;********** PROGRAM-LOOP ********
+   CLR  R16
+LOOP:
+   LDI  R17,9           ;R17 = 9 
+   ADD  R16,R17         ;R16 = R16 + R17
+   CALL DISP_AND_DELAY  ;Display R16
+   JMP  LOOP            ;Jump to "LOOP"
+
+;********** DISPLAY R16 *********
+;********** AND DELAY ***********
+DISP_AND_DELAY:
+   MOV  R17,R16
+   OUT  PORTB,R17
+   CLR  R17
+   CLR  R18
+   LDI  R19,100
+AGAIN:
+   DEC  R17
+   BRNE AGAIN
+   DEC  R18
+   BRNE AGAIN
+   DEC  R19
+   BRNE AGAIN
+   RET	
+;********************************
